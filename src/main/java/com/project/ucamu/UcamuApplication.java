fe177c6b317ccc12587bccd5951cd5fe4d4c45fa
KBSure <kbsure.dev@gmail.com>
2018-05-28 11:58:25 +0900

    application.yml 설정

---
+++ src/main/java/com/project/ucamu/UcamuApplication.java
@@ -0,0 +1,12 @@
+package com.project.ucamu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcamuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcamuApplication.class, args);
+	}
+}
---
+++ src/test/java/com/project/ucamu/UcamuApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.ucamu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UcamuApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
