d4a437b43a5b23ad2be7fb07a082eb34a5956079
KBSure <kbsure.dev@gmail.com>
2018-06-02 23:09:29 +0900

    layout 나누기

---
+++ src/main/java/com/project/ucamu/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.project.ucamu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String getTest(){
+        return "test";
+    }
+}
